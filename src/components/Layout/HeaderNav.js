4def91969b6bfa8d4b453a2fc1e2a5aa2e9162de
CREIFT7 <farh062098@gmail.com>
2026-02-24 18:41:42 -0800

    Second commit
    Modificacion al header y agregado de Imagenes background y un poco de queries

--- src/components/Layout/HeaderNav.js
+++ src/components/Layout/HeaderNav.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logoB from "../../assets/Images/LogoBufalo.png";
 
 export const HeaderNav = () =>{
 
     return (
-        <header>
+        <header className='header'>
+            <div className='logo'>
+                <img src={logoB} alt="Fredy Rodriguez Logo" className="logo-img" />
+                <h3>
+                    FREDY RODRIGUEZ 
+                    <br>
+                    </br>
+                    JR.SOFTWARE DEV.
+                </h3>
+            </div>
+
             <nav>
                 <ul>
                     <li>
